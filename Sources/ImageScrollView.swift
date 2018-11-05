a83403694b8382f138e76cd1ed9daffed026b693
Huy Nguyen <conghuy2012@gmail.com>
2018-11-05 16:10:18 +0700

    Add imageScrollViewDelegate property, so that user can set delegate to catch did scroll, did zoom... event. It is a UIScrollViewDelegate.

--- Sources/ImageScrollView.swift
+++ Sources/ImageScrollView.swift
@@ -28,6 +28,8 @@ open class ImageScrollView: UIScrollView {
     @objc open var initialOffset: Offset = .begining
     
     @objc public private(set) var zoomView: UIImageView? = nil
+    
+    @objc open weak var imageScrollViewDelegate: UIScrollViewDelegate?
 
     var imageSize: CGSize = CGSize.zero
     private var pointToCenterAfterResize: CGPoint = CGPoint.zero
@@ -268,14 +270,60 @@ open class ImageScrollView: UIScrollView {
     }
 }
 
-extension ImageScrollView: UIScrollViewDelegate{
+extension ImageScrollView: UIScrollViewDelegate {
     
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        imageScrollViewDelegate?.scrollViewDidScroll?(scrollView)
+    }
+
+    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        imageScrollViewDelegate?.scrollViewWillBeginDragging?(scrollView)
+    }
+
+    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        imageScrollViewDelegate?.scrollViewWillEndDragging?(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+    }
+    
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        imageScrollViewDelegate?.scrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
+    }
+    
+    public func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        imageScrollViewDelegate?.scrollViewWillBeginDecelerating?(scrollView)
+    }
+    
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        imageScrollViewDelegate?.scrollViewDidEndDecelerating?(scrollView)
+    }
+    
+    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        imageScrollViewDelegate?.scrollViewDidEndScrollingAnimation?(scrollView)
+    }
+    
+    public func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        imageScrollViewDelegate?.scrollViewWillBeginZooming?(scrollView, with: view)
+    }
+    
+    public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        imageScrollViewDelegate?.scrollViewDidEndZooming?(scrollView, with: view, atScale: scale)
+    }
+    
+    public func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+        return false
+    }
+    
+    @available(iOS 11.0, *)
+    public func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) {
+        imageScrollViewDelegate?.scrollViewDidChangeAdjustedContentInset?(scrollView)
+    }
+
     public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return zoomView
     }
     
     public func scrollViewDidZoom(_ scrollView: UIScrollView) {
         adjustFrameToCenter()
+        imageScrollViewDelegate?.scrollViewDidZoom?(scrollView)
     }
     
 }
